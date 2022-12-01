998c044528a2d2ad7f3926f5196bbf2fba822108
gaurav_rudraserver <gaurav@rudrainnovatives.com>
2022-12-01 19:02:11 +0530

    navigation

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,33 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+  
+  
+  <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 justify-content-between">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    
+    </ul>
+  </div>
+</nav>
+
+
+
+
+
+
   )
 }
