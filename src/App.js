3752dfded9a120f1e4f756557b56f3cc32280023
gaurav_rudraserver <gaurav@rudrainnovatives.com>
2022-12-02 15:58:24 +0530

    html and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+
 import Header from './components/common/header';
 import { RouterProvider } from 'react-router-dom';
 import Footer from './components/common/footer';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './assets/css/style.css'
+import './App.css';
 function App({router}) {
   return (
     <div>
