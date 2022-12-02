016e60badbde553405c05c3bf1e7c04e4a933df0
gaurav_rudraserver <gaurav@rudrainnovatives.com>
2022-12-02 12:55:59 +0530

    navigation

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,27 +3,54 @@ import React from 'react'
 export default function About() {
   return (
   
+
+  <div>  
   
-  <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 justify-content-between">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
+    <nav className="navbar navbar-expand-lg  border-bottom">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#"></a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link" href="#">API</a>
+          <a className="nav-link" href="#">Explore</a>
+        <a href="#" className="mx-4  btn btn-primary text-uppercase font-nunito btn-theme">Login</a>
+        <a href="#" className="btn btn-danger text-uppercase font-nunito btn-theme">Signup</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+
+  <footer className="bg-dark text-white py-5 mt-5 text-center">
+        <div className="container py-4">
+        <h3 className="fw-semibold mb-0">Touchupp</h3>
     
-    </ul>
+        <div className="footer-link">
+            <ul className="list-unstyled list-inline my-5">
+                <li className="list-inline-item mx-4"><a href="#" className="font-nunito text-white text-decoration-none">Home</a></li>
+                <li className="list-inline-item mx-4"><a href="#" className="font-nunito text-white text-decoration-none">Pricing</a></li>
+                <li className="list-inline-item mx-4"><a href="#" className="font-nunito text-white text-decoration-none">About us</a></li>
+                <li className="list-inline-item mx-4"><a href="#" className="font-nunito text-white text-decoration-none">Contact</a></li>
+                <li className="list-inline-item mx-4"><a href="#" className="font-nunito text-white text-decoration-none">Privacy</a></li>
+                <li className="list-inline-item mx-4"><a href="#" className="font-nunito text-white text-decoration-none">Terms of Use</a></li>
+
+            </ul>
+        </div>
+        <p>© 2022-2023, All Rights Reserved</p>
+        </div>
+    </footer>
+
   </div>
-</nav>
+
+
+ 
+
+
+
 
 
 
