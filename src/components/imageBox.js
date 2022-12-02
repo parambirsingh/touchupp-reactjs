2b26ac69676cc318901cb662679f98d8caa7c1de
TaseenAnsari <taseensaif@gmal.com>
2022-12-02 16:26:46 +0530

    all done

--- src/components/imageBox.js
+++ src/components/imageBox.js
@@ -2,18 +2,20 @@ import React, { useEffect, useRef, useState } from 'react'
 import { toast } from 'react-toastify'
 import { getImage } from '../services/imageServices'
 import Draggable from 'react-draggable';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 
 let originalHeight = 486
 let originalWidth = 864
 let originalCoord = []
-function ImageBox({ image, coord, setCoord, setImageDimension,setImage2Dimension ,scale}) {
+function ImageBox({ image, coord, setCoord, setImageDimension, setImage2Dimension, scale }) {
     const imageRef = useRef()
     const boxRef = useRef()
     let xStart = 100
     let yStart = 100
 
     const handleResize = () => {
+        if (!imageRef || !boxRef) return
         let arr = [imageRef.current.clientWidth, imageRef.current.clientHeight]
         setImage2Dimension([imageRef.current.clientWidth, imageRef.current.clientHeight])
         setImageDimension(arr)
@@ -34,6 +36,7 @@ function ImageBox({ image, coord, setCoord, setImageDimension,setImage2Dimension
         setCoord(coords)
     }
     useEffect(() => {
+        if (!imageRef) return
         originalHeight = imageRef.current.naturalHeight
         originalWidth = imageRef.current.naturalWidth
         window.addEventListener('resize', handleResize);
@@ -45,7 +48,9 @@ function ImageBox({ image, coord, setCoord, setImageDimension,setImage2Dimension
     useEffect(() => {
         if (originalCoord.length == 0) originalCoord = JSON.parse(JSON.stringify(coord))
         // getImageData()
-        handleResize()
+        setTimeout(() => {
+            handleResize()
+        })
         //   getImageData();
     }, [])
     const getImageData = async () => {
@@ -59,17 +64,28 @@ function ImageBox({ image, coord, setCoord, setImageDimension,setImage2Dimension
     };
 
     return (
-        <Draggable className='w-auto'>
-            <div ref={boxRef} className='mt-2 h-max-80vh d-flex justify-content-center position-relative' style={{ transform: `scale(${scale})`}}>
+
+        <div className=''>
+            {/* <TransformWrapper
+            // initialScale={1}
+            // initialPositionX={0}
+            // initialPositionY={0}
+            >
+                <TransformComponent >
+                </TransformComponent>
+            </TransformWrapper> */}
+            <div ref={boxRef} className='mt-2 h-max-80vh d-flex justify-content-center position-relative'
+                style={{ transform: `scale(${scale})` }}
+            >
                 {coord.map((c) => (<div className='position-absolute' key={c.key} style={{ top: (c.coordinates[1]) + 'px', left: c.coordinates[0] + 'px' }}>
                     <span className='hover-danger cursor-pointer'>
                         <i className="bi bi-x-circle-fill"></i>
                         {c.key}
                     </span>
                 </div>))}
-                <img ref={imageRef} src={image} className='h-max-80vh object-fit' style={{ objectFit: 'contain', maxWidth: '100%'  }} />
+                <img ref={imageRef} src={image} className='h-max-80vh object-fit' style={{ objectFit: 'contain', maxWidth: '100%' }} />
             </div>
-        </Draggable>
+        </div>
     )
 }
 
