4f9472ef00d67223e5d4d8394b6ddc2459106189
prabhatm102 <prabhat@rudrainnovative.in>
2022-12-09 18:17:44 +0530

    Test

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -26,97 +26,7 @@ function Canvas({
       setPaths([])
     }
   }, [brushedImage]);
-  const draw = (ctx, img, paths) => {
-    ctx.drawImage(img, 0, 0);
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fill();
-    // ctx.save();
 
-    // let xStart = (boxRef.current.clientWidth - img.width) / 2;
-    // let yStart = (boxRef.current.clientHeight - img.height) / 2;
-
-    //  ctx.translate(0.5, 0.5);
-    ctx.beginPath();
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-    ctx.lineWidth = brushData.brushStock;
-
-    ctx.moveTo(paths[0]?.x, paths[0]?.y);
-    for (let i = 1; i < paths?.length; i++) {
-      // let decreaseY = (paths[i]?.y / 100) * percentDecreaseHeight;
-      // let decreaseX = (paths[i]?.x / 100) * percentDecreaseWidth;
-
-      // paths[i].y =  paths[i]?.y + decreaseY;
-      // paths[i].x =  paths[i]?.x + decreaseX;
-
-      //  ctx.moveTo(paths[i]?.x, paths[i]?.y);
-      ctx.lineTo(Math.round(paths[i]?.x), Math.round(paths[i]?.y));
-    }
-    ctx.closePath();
-    ctx.strokeStyle = "white";
-    ctx.stroke();
-
-    // let percentDecreaseHeight =
-    //   100 - (rect.height / img.height) * 100;
-    // let percentDecreaseWidth =
-    //   100 - (rect.width / img.width) * 100;
-
-    // let decreaseY = (paths[0]?.y / 100) * percentDecreaseHeight;
-    // let decreaseX = (paths[0]?.x / 100) * percentDecreaseWidth;
-    // // console.log(paths[0]?.x, paths[0]?.y);
-    // paths[0].y =   paths[0]?.y +decreaseY;
-    // paths[0].x =  paths[0]?.x + decreaseX;
-
-    // // console.log(decreaseY, decreaseX);
-    // ctx.moveTo(paths[0]?.x, paths[0]?.y);
-    // //  console.log(paths[0]?.x, paths[0]?.y)
-    // //   ctx.lineTo(paths[0 + 1]?.x, paths[0 + 1]?.y);
-    // // ctx.moveTo(paths[i]?.x, paths[i]?.y);
-
-    // for (let i = 1; i < paths?.length; i++) {
-    //   let decreaseY = (paths[i]?.y / 100) * percentDecreaseHeight;
-    //   let decreaseX = (paths[i]?.x / 100) * percentDecreaseWidth;
-
-    //   paths[i].y =  paths[i]?.y + decreaseY;
-    //   paths[i].x =  paths[i]?.x + decreaseX;
-
-    //   //  ctx.moveTo(paths[i]?.x, paths[i]?.y);
-    //   ctx.lineTo(Math.round(paths[i]?.x), Math.round(paths[i]?.y));
-    // }
-    // ctx.closePath();
-    // ctx.strokeStyle = "blue";
-    // ctx.stroke();
-    // ctx.clip();
-
-    // //  ctx.fillStyle = "white";
-    // //  ctx.fill();
-    // ctx.restore();
-
-    var jpegUrl = ctx.canvas.toDataURL("image/jpeg");
-    // console.log(jpegUrl);
-    let brushedSrc = jpegUrl.slice(jpegUrl?.indexOf(",") + 1);
-    setBrushedImage(brushedSrc);
-    // canvas.current?.clearCanvas();
-  };
-
-  let setTimoutHandle;
-
-  const removeSelectedPath = (paths) => {
-    // console.log(paths)
-    let dyanmicCanvas = document.createElement("CANVAS");
-    // dyanmicCanvas.height = 614;
-    // dyanmicCanvas.width = 1024;
-    var img = document.createElement("IMG");
-    img.onload = function () {
-      dyanmicCanvas.height = img.height;
-      dyanmicCanvas.width = img.width;
-      let ctx = dyanmicCanvas.getContext("2d");
-      // console.log(img.height, img.width);
-      draw(ctx, img, paths, dyanmicCanvas);
-    };
-    img.src = imageData.base64Start + imageData.originalImage;
-  };
 
   const handlePath = async () => {
     if (canvas?.current) {
@@ -179,12 +89,12 @@ function Canvas({
         <ReactSketchCanvas
           height={actualDimention.height}
           width={actualDimention.width}
-          className={" " + (isBrushing ? "loading-image" : "")}
+          className={"cursor-area " + (isBrushing ? "loading-image" : "")}
           ref={canvas}
           style={{
             // height: canvasDimention?.height,
             // width: canvasDimention?.width,
-            objectFit: "contain",
+            objectFit: "cover",
             margin: "0 auto",
             cursor: "none",
           }}
