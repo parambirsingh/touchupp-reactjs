140291a0d4e20ed6fa1e62da8b16da818219312e
TaseenAnsari <taseensaif@gmal.com>
2022-12-12 13:27:46 +0530

    changes done

--- src/components/uploadImage.js
+++ src/components/uploadImage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import headingImg from "../assets/img/underline.svg";
 import { ImageContext } from "../context/imageContext";
 // import { Constants } from '../data/constants';
