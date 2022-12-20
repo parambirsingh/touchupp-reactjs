9e20b27a3b415600866cde13e53c77c8f9b73815
Taseen Ansari <taseen@rudrainnovative.in>
2022-12-20 16:10:00 +0530

    canvas fix

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -20,70 +20,70 @@ function Canvas({
   const canvas = useRef();
   const boxRef = useRef();
 
-    const handleDone = (paths) => {
-      if (!paths?.length || !paths?.[0]?.paths?.length) return;
-      removeSelectedPath(paths);
-    };
+  const handleDone = (paths) => {
+    if (!paths?.length || !paths?.[0]?.paths?.length) return;
+    removeSelectedPath(paths);
+  };
 
-    const draw = (ctx, img,paths) => {
-      let canvaHeight = getDimention()?.height;
-      let canvaWidth = getDimention()?.width;
-      let rx = ctx.canvas.width / canvaWidth;
-      let ry = ctx.canvas.height / canvaHeight;
+  const draw = (ctx, img, paths) => {
+    let canvaHeight = getDimention()?.height;
+    let canvaWidth = getDimention()?.width;
+    let rx = ctx.canvas.width / canvaWidth;
+    let ry = ctx.canvas.height / canvaHeight;
 
-      let modifiedPaths = paths?.map((p) => {
-        let temp = { ...p };
-        temp.paths = temp?.paths?.map((q) => {
-          let tempQ = {};
-          tempQ.x = Math.round(q?.x * rx);
-          tempQ.y = Math.round(q?.y * ry);
-          return tempQ;
-        });
-        return temp;
+    let modifiedPaths = paths?.map((p) => {
+      let temp = { ...p };
+      temp.paths = temp?.paths?.map((q) => {
+        let tempQ = {};
+        tempQ.x = Math.round(q?.x * rx);
+        tempQ.y = Math.round(q?.y * ry);
+        return tempQ;
       });
-      ctx.drawImage(img, 0, 0);
-      ctx.fillStyle = "black";
-      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      ctx.fill();
-      for (let item of modifiedPaths) {
-        ctx.beginPath();
-        ctx.lineCap = "round";
-        ctx.lineJoin = "round";
-        ctx.lineWidth = item.strokeWidth * rx;
+      return temp;
+    });
+    ctx.drawImage(img, 0, 0);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fill();
+    for (let item of modifiedPaths) {
+      ctx.beginPath();
+      ctx.lineCap = "round";
+      ctx.lineJoin = "round";
+      ctx.lineWidth = item.strokeWidth * rx;
 
-        ctx.moveTo(item?.paths[0]?.x, item?.paths[0]?.y);
-        for (let i = 1; i < item?.paths?.length; i++) {
-          ctx.lineTo(
-            Math.round(item?.paths[i]?.x),
-            Math.round(item?.paths[i]?.y)
-          );
-        }
-        // ctx.closePath();
-        ctx.strokeStyle = "white";
-        ctx.stroke();
+      ctx.moveTo(item?.paths[0]?.x, item?.paths[0]?.y);
+      for (let i = 1; i < item?.paths?.length; i++) {
+        ctx.lineTo(
+          Math.round(item?.paths[i]?.x),
+          Math.round(item?.paths[i]?.y)
+        );
       }
+      // ctx.closePath();
+      ctx.strokeStyle = "white";
+      ctx.stroke();
+    }
 
-      var jpegUrl = ctx.canvas.toDataURL("image/jpeg");
-      //     var newTab = window.open();
-      // newTab.document.body.innerHTML = `<img src="${jpegUrl}`
-      let brushedSrc = jpegUrl.slice(jpegUrl?.indexOf(",") + 1);
-      setDrawPath(modifiedPaths);
-      setBrushedImage(brushedSrc);
+    var jpegUrl = ctx.canvas.toDataURL("image/jpeg");
+    //     var newTab = window.open();
+    // newTab.document.body.innerHTML = `<img src="${jpegUrl}`
+    let brushedSrc = jpegUrl.slice(jpegUrl?.indexOf(",") + 1);
+    setDrawPath(modifiedPaths);
+    setBrushedImage(brushedSrc);
+  };
+
+  const removeSelectedPath = (paths) => {
+    let dyanmicCanvas = document.createElement("CANVAS");
+    var img = document.createElement("IMG");
+    img.onload = function () {
+      dyanmicCanvas.height = img.height;
+      dyanmicCanvas.width = img.width;
+      let ctx = dyanmicCanvas.getContext("2d");
+      draw(ctx, img, paths);
     };
+    img.src = imageData.base64Start + imageData?.originalImage;
+  };
 
-    const removeSelectedPath = (paths) => {
-      let dyanmicCanvas = document.createElement("CANVAS");
-      var img = document.createElement("IMG");
-      img.onload = function () {
-        dyanmicCanvas.height = img.height;
-        dyanmicCanvas.width = img.width;
-        let ctx = dyanmicCanvas.getContext("2d");
-        draw(ctx, img,paths);
-      };
-      img.src = imageData.base64Start + imageData?.originalImage;
-    };
 
- 
   useEffect(() => {
     if (!brushedImage) {
       canvas.current?.clearCanvas();
@@ -99,15 +99,15 @@ function Canvas({
   }, [imageDimension]);
 
   const handlePath = async () => {
-    if(isBrushing) return;
+    if (isBrushing) return;
     if (canvas?.current) {
       let path = await canvas?.current?.exportPaths();
-     
-      if (path?.[0]?.paths){
+
+      if (path?.[0]?.paths) {
         // path[0].height = getDimention()?.height;
         // path[0].width = getDimention()?.width;
-       handleDone(path)
-      } 
+        handleDone(path)
+      }
       // clearTimeout(setTimoutHandle);
       // setTimoutHandle = setTimeout(async () => {
       //   let path;
@@ -141,37 +141,37 @@ function Canvas({
       // }, 1000);
     }
   };
- 
+
   const getDimention = () => {
     return imageDimension?.width > 1000
       ? window?.innerWidth >= Constants?.screenDimensions?.xl?.screenWidth
         ? Constants?.screenDimensions?.xl
         : window?.innerWidth >= Constants?.screenDimensions?.lg?.screenWidth
-        ? Constants?.screenDimensions?.lg
-        : window?.innerWidth >= Constants?.screenDimensions?.md?.screenWidth
-        ? Constants?.screenDimensions?.md
-        : Constants?.screenDimensions?.sm
+          ? Constants?.screenDimensions?.lg
+          : window?.innerWidth >= Constants?.screenDimensions?.md?.screenWidth
+            ? Constants?.screenDimensions?.md
+            : Constants?.screenDimensions?.sm
       : imageDimension;
   };
   return (
-    <div className="h-100 d-flex justify-content-center align-items-center w-100">
+    <div className="h-100 d-flex justify-content-center align-items-center w-100 overflow-hidden">
+      <TailSpin
+        height="50"
+        width="50"
+        color="#dc3545"
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{ zIndex: 10001 }}
+        wrapperClass="position-absolute justify-content-center align-items-center h-100 w-100"
+        visible={isBrushing}
+      />
       <div
         ref={boxRef}
         className="row mx-0 align-items-center justify-content-center position-relative h-100 hover-effect"
         onMouseUp={() => handlePath()}
         onTouchStart={() => handlePath()}
-        // onMouseOut={() => handlePath()}
+      // onMouseOut={() => handlePath()}
       >
-        <TailSpin
-          height="50"
-          width="50"
-          color="#dc3545"
-          ariaLabel="tail-spin-loading"
-          radius="1"
-          wrapperStyle={{ zIndex: 10001 }}
-          wrapperClass="position-absolute justify-content-center align-items-center h-100 w-100"
-          visible={isBrushing}
-        />
 
         <ReactSketchCanvas
           height={getDimention()?.height}
@@ -204,8 +204,8 @@ function Canvas({
             opacity: 0.7,
           }}
           opacity={0}
-          // targetScale={1}
-          // targetOpacity={0.7}
+        // targetScale={1}
+        // targetOpacity={0.7}
         />
       </div>
     </div>
